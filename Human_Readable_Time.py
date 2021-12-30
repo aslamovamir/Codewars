73b1e27ab80f6c2a151b7926d22c41a0af0ae96e
Amir Aslamov <90361791+aslamovamir@users.noreply.github.com>
2021-12-29 20:02:34 -0500

    Create Human_Readable_Time.py

---
+++ Human_Readable_Time.py
@@ -0,0 +1,22 @@
+# Write a function, which takes a non-negative integer (seconds) as input and returns the time in a human-readable format (HH:MM:SS)
+
+# HH = hours, padded to 2 digits, range: 00 - 99
+# MM = minutes, padded to 2 digits, range: 00 - 59
+# SS = seconds, padded to 2 digits, range: 00 - 59
+# The maximum time never exceeds 359999 (99:59:59)
+
+# You can find some examples in the test fixtures.
+
+def make_readable(seconds):
+    hours = seconds//3600
+    minutes = (seconds//60)%60
+    seconds = ((seconds%60)%60)%60
+    
+    if hours < 10:
+        hours = f"0{hours}"
+    if minutes < 10:
+        minutes = f"0{minutes}"
+    if seconds < 10:
+        seconds = f"0{seconds}"
+    
+    return f"{hours}:{minutes}:{seconds}"
